32dfd420e1f20fddda2cb8022d0161d94c8418b9
Demerak <alexandre_poirier@outlook.com>
2024-02-10 15:13:16 -0500

    Remove unnecessary code, and gain a better understanding of Tauri by reading documentation which led me to restrict the allowlist of API

--- app/page.tsx
+++ app/page.tsx
@@ -3,61 +3,27 @@ import Image from "next/image";
 import React, { useState } from "react";
 import styles from "./page.module.css";
 import { invoke } from "@tauri-apps/api/tauri";
+import { open } from "@tauri-apps/api/dialog";
 
-async function invokeTauriCommand(command: any) {
-  return invoke("tauri", command);
-}
+// async function invokeTauriCommand(command: any) {
+//   return invoke("tauri", command);
+// }
 
 export default function Home() {
-
   const openFileExplorer = async () => {
     console.log("Open File Explorer");
-    open();
-  }
-
-  interface DialogFilter {
-    /** Filter name. */
-    name: string
-    /**
-     * Extensions to filter, without a `.` prefix.
-     * @example
-     * ```typescript
-     * extensions: ['svg', 'png']
-     * ```
-     */
-    extensions: string[]
-  }
-
-  interface OpenDialogOptions {
-    /** The title of the dialog window. */
-    title?: string
-    /** The filters of the dialog. */
-    filters?: DialogFilter[]
-    /** Initial directory or file path. */
-    defaultPath?: string
-    /** Whether the dialog allows multiple selection or not. */
-    multiple?: boolean
-    /** Whether the dialog is a directory selection or not. */
-    directory?: boolean
-    /**
-     * If `directory` is true, indicates that it will be read recursively later.
-     * Defines whether subdirectories will be allowed on the scope or not.
-     */
-    recursive?: boolean
-  }
-
-  async function open(options: OpenDialogOptions = {}): Promise<null | string | string[]> {
-    if (typeof options === 'object') {
-      Object.freeze(options)
-    }
-    let res = invokeTauriCommand({
-      __tauriModule: 'Dialog',
-      message: {
-        cmd: 'openDialog',
-        options
-      }
+    const file = await open({
+      multiple: false,
+      directory: false,
+      filters: [
+        {
+          // only display pdf files
+          name: "PDF",
+          extensions: ["pdf"],
+        },
+      ],
     })
-    return res as any;
+    console.log(file);
   }
 
   return (
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{ops::Deref, process::Command, sync::Mutex};
-
-#[tauri::command]
-fn show_in_folder(path: String) {
-  #[cfg(target_os = "windows")]
-  {
-    Command::new("explorer")
-      .args(["/select,", &path]) 
-      .spawn()
-      .unwrap();
-  }
-}
-
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
